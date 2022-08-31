02821a9d96389067421166fd79064a7e7b47e1e4
Kodanda <kodandareddy21@gmail.com>
2022-08-31 15:56:56 +0530

    live links added

--- src/Components/Projects/project.js
+++ src/Components/Projects/project.js
@@ -4,96 +4,96 @@ export const projects=[
         id:1,
         img_path:"/images/1.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-01",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Street_style_page_1",
+        live_project:"https://630c4df595131e56b7ab980c--frolicking-dusk-892b07.netlify.app/",
         title:"Street Side landing Page"
     },
     {
         id:2,
         img_path:"/images/2.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-2",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Restaurent_homepage_2",
+        live_project:"https://sparkling-profiterole-fcadec.netlify.app/",
         title:"Food Restaurant  Page"
     },
     {
         id:3,
         img_path:"/images/3.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-3-forked",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Law_home_page_3",
+        live_project:"https://rainbow-alpaca-43e049.netlify.app/",
         title:"Law Home Page"
     },
     {
         id:4,
         img_path:"/images/4.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-04",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/digitalmarketing_homepage_4",
+        live_project:"https://loquacious-vacherin-5a57db.netlify.app/",
         title:"Digital Marketing Homepage"
     },
     {
         id:5,
         img_path:"/images/5.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-5",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/crypto_landingpage_5",
+        live_project:"https://spontaneous-druid-38acc6.netlify.app/",
         title:"Crypto Landing page"
     },
     {
         id:6,
         img_path:"/images/6.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-6",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/plant_homepage_6",
+        live_project:"https://majestic-muffin-f60775.netlify.app/",
         title:"Plant HomePage.png"
     },
     {
         id:7,
         img_path:"/images/7.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-7",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/product_homepage_7",
+        live_project:"https://graceful-starship-9905fc.netlify.app/",
         title:"Product Home Page"
     },
     {
         id:8,
         img_path:"/images/8.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-8",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Web_design_8",
+        live_project:"https://jazzy-stardust-03bd10.netlify.app/",
         title:"Web Design Landing Page"
     },
     {
         id:9,
         img_path:"/images/9.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/live-class-project-9",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Developer_landing_page_9",
+        live_project:"https://peppy-froyo-523525.netlify.app/",
         title:"Developer Landing Page"
     },
     {
         id:10,
         img_path:"/images/10.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/project-10",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Interior_design_10",
+        live_project:"https://deluxe-kashata-bc8cd8.netlify.app/",
         title:"Interior Design  Page"
     },
     {
         id:11,
         img_path:"/images/11.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/project-11",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/Developer_homepage_11",
+        live_project:"https://roaring-otter-62b392.netlify.app/",
         title:"Hosting Landing Page"
     },
     {
         id:12,
         img_path:"/images/12.png",
         badge:["Html","Css"],
-        github_link:"https://github.com/kodandareddy/Js-full-stack/tree/main/Html%26css/project12",
-        live_project:"",
+        github_link:"https://github.com/kodandareddy/business_landingPage_12",
+        live_project:"https://lucent-quokka-fb7820.netlify.app/",
         title:"Business Landing Page"
     },
     {
@@ -108,8 +108,8 @@ export const projects=[
         id:14,
         img_path:"/images/14.png",
         badge:["Html","Css","tailwind"],
-        github_link:"https://github.com/kodandareddy/expert-clone",
-        live_project:"https://ineuron-home-page.vercel.app/",
+        github_link:"https://github.com/kodandareddy/paytmClone",
+        live_project:"https://paytmclone.vercel.app/",
         title:"Paytm Clone "
     },
     {
@@ -119,6 +119,39 @@ export const projects=[
         github_link:"https://github.com/kodandareddy/Shopify",
         live_project:"https://shopify-eosin.vercel.app/",
         title:"Shopify Clone "
+    },
+    {
+        id:16,
+        img_path:"/images/16.jpg",
+        badge:["Html","Css","tailwind"],
+        github_link:"https://github.com/kodandareddy/rode_clone",
+        live_project:"https://rode-clone-eta.vercel.app/",
+        title:"Rode Clone "
+    },
+    {
+        id:17,
+        img_path:"/images/17.jpg",
+        badge:["Html","Css"],
+        github_link:"https://github.com/kodandareddy/SASS_landingpage_13",
+        live_project:"https://js-full-stack-jgzeg9f7k-kodandareddy.vercel.app/",
+        title:"SASS landing Page"
+    },
+    {
+        id:18,
+        img_path:"/images/18.jpg",
+        badge:["Html","Css"],
+        github_link:"https://github.com/kodandareddy/Dance_Home_Page_14",
+        live_project:"https://wondrous-dragon-69d390.netlify.app/",
+        title:"Dance Home Page"
+    },
+    {
+        id:19,
+        img_path:"/images/19.jpg",
+        badge:["Html","Css"],
+        github_link:"https://github.com/kodandareddy/ProductDesignPage15",
+        live_project:"https://jade-seahorse-cfe55d.netlify.app/",
+        title:"Product Design Page"
     }
-    
+
+
 ]
